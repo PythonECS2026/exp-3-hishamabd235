6b13f57b83464272b0e946fdfa60e21dac3c8cc0
Hisham Shariq Abdullah <65910788+hishamabd235@users.noreply.github.com>
2026-02-19 22:57:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
-# Coder:
-# Date:
+# Coder: Hisham Shariq Abdullah
+# Date: 19/02/26
 
 # Write your code here
+bs = float(input("Enter Basic Salary: "))
+
+da = 0.70 * bs
+ta = 0.30 * bs
+hra = 0.10 * bs
+gross_salary = bs + da + ta + hra
+
+print("\nSalary Details:")
+print(f"Basic Salary:  {bs}")
+print(f"DA (70%):      {da}")
+print(f"TA (30%):      {ta}")
+print(f"HRA (10%):     {hra}")
+print(f"Gross Salary:  {gross_salary}")
